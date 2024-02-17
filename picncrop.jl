ad007bd1b9f3ab6c8008e190327987124572290c
sardinecan <morvan.josselin@gmail.com>
2024-02-17 20:42:08 +0100

    reviewed picncrop script

--- picncrop.jl
+++ picncrop.jl
@@ -9,8 +9,12 @@ fig = Figure()
 
 i_slice = Observable(1)
 
-ax = Axis(fig[1, 1], aspect = DataAspect(), yreversed = true,
-    title = @lift("Slice $($i_slice)"))
+ax =    Axis(
+            fig[1, 1],
+            aspect = DataAspect(), 
+            yreversed = true,
+            title = @lift("Slice $($i_slice)")
+        )
 hidedecorations!(ax)
 
 chosen_slice = @lift(images[:, :, $i_slice]')
@@ -25,9 +29,12 @@ b3 = Button(buttongrid[3, 1], label = "Previous image")
 
 on(b.clicks) do c
     lims = ax.finallimits[]
-    println("save image cropped to $lims")
-    println("save image cropped to $ax")
-    save("ouput.png", lims)
+    
+    mini, maxi = extrema(lims) # get the bottom left and top right corners of `lims`
+    mini = round.(Int, mini) # to index into an image, which is an array, we need ints
+    maxi = round.(Int, maxi) # and the axis bounding box is always represented in floats
+    cropped_slice = chosen_slice[][mini[1]:maxi[1], mini[2]:maxi[2]]
+    save("output_$(i_slice[]).png", cropped_slice)
 end
 
 on(b2.clicks) do c
