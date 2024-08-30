eba60e37df249643693d62abff2e062f8f96e13a
sardinecan <morvan.josselin@gmail.com>
2024-08-30 10:34:23 +0200

    reviewed app to display and save image with image name

--- picncrop.jl
+++ picncrop.jl
@@ -12,13 +12,25 @@ images = [load(image) for image in input]
 fig = Figure()
 
 image = Observable(1)
+imageName = @lift(basename(input[$image]))
 
-ax =    Axis(
-            fig[1, 1],
-            aspect = DataAspect(), 
-            yreversed = true,
-            title = @lift("Image $($image)")
-        )
+function getUniqueFilename(base_name, output_path)
+  ext = split(base_name, ".")[end] # Extraire l'extension du fichier
+  filename = "$(output_path)/$(base_name).crop.$(ext)"
+  counter = 1
+  while isfile(filename)
+    filename = "$(output_path)/$(base_name).crop_$(counter).$(ext)"
+    counter += 1
+  end
+  return filename
+end
+
+ax = Axis(
+  fig[1, 1],
+  aspect = DataAspect(), 
+  yreversed = true,
+  title = image_name
+)
 hidedecorations!(ax)
 
 selectediImage = @lift(images[$image]')
@@ -33,29 +45,29 @@ b3 = Button(buttongrid[3, 1], label = "Previous image")
 b4 = Button(buttongrid[4, 1], label = "Reload")
 
 on(b.clicks) do c
-    lims = ax.finallimits[]
-    
-    mini, maxi = extrema(lims) # get the bottom left and top right corners of `lims`
-    mini = round.(Int, mini) # to index into an image, which is an array, we need ints
-    maxi = round.(Int, maxi) # and the axis bounding box is always represented in floats
-    croppedImage = selectediImage[][mini[1]:maxi[1], mini[2]:maxi[2]]
-    pos = parse(Int64, "$(image[])")
-    ext = split(input[pos], ".")[2]
-    save(path*"/output/image_$(image[])."*ext, rotl90(croppedImage[:, end:-1:1]))
-end
+  lims = ax.finallimits[]
+  mini, maxi = extrema(lims) # get the bottom left and top right corners of `lims`
+  mini = round.(Int, mini) # to index into an image, which is an array, we need ints
+  maxi = round.(Int, maxi) # and the axis bounding box is always represented in floats
+  croppedImage = selectediImage[][mini[1]:maxi[1], mini[2]:maxi[2]]
+  filename = getUniqueFilename(image_name[], path*"/output")
+  println("saved "*filename)
+  save(filename, rotl90(croppedImage[:, end:-1:1]))
+end 
 
 on(b2.clicks) do c
-    image[] = mod1(image[] + 1, length(input))
-    autolimits!(ax)
+  image[] = mod1(image[] + 1, length(input))
+  autolimits!(ax)
 end
 
 on(b3.clicks) do c
-    image[] = mod1(image[] - 1, length(input))
-    autolimits!(ax)
+  image[] = mod1(image[] - 1, length(input))
+  autolimits!(ax)
 end
+
 on(b4.clicks) do c
-    image[] = mod1(image[], length(input))
-    autolimits!(ax)
+  image[] = mod1(image[], length(input))
+  autolimits!(ax)
 end
 
 fig
